f646d6688717ee866ee2b2e90dd4a0d1e174e7c2
dangquochung <dangquochung.ptit@gmail.com>
2024-01-25 17:39:25 +0700

    Add tabbar view and tableview for homeviewcontroller

---
+++ Netflix Clone/Controllers/DownloadViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadViewController.swift
+//  Netflix Clone
+//
+//  Created by Dang Hung on 25/01/2024.
+//
+
+import UIKit
+
+class DownloadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+    }
+
+}
---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Dang Hung on 25/01/2024.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+    }
+
+}
